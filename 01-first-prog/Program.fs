edcfd5ab310d64fa0cbdc7bee68c09c783e00082
Rafael Lefever <rafael.lefever@discomco.pl>
2022-08-14 22:45:35 +0200

    initial commit

---
+++ 01-first-prog/Program.fs
@@ -0,0 +1,9 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System
+
+[<EntryPoint>]
+let main argv =  // string[] -> int
+    printfn "Hello from F#"
+    printfn "The args are: %A" argv
+    0
