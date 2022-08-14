89a108a1ac6ee5d1efd1d2281587be4384a9a6ed
Rafael Lefever <rafael.lefever@discomco.pl>
2022-08-14 22:55:22 +0200

    2nd lesson

--- 02-conditional-branching/Program.fs
+++ 02-conditional-branching/Program.fs
@@ -4,6 +4,8 @@ open System
 
 [<EntryPoint>]
 let main argv =
-    let person = argv[0]
-    printfn "Hello from %s" person
-    0
+  let mutable person = "John"
+  if argv.Length >0 then
+     person <- argv[0]
+  printfn "Hello from %s" person
+  0
