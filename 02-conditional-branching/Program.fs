4bcd33c9e321920d2ea66970af4db89390e24b8a
Rafael Lefever <rafael.lefever@discomco.pl>
2022-08-14 22:51:01 +0200

    lesson 2

---
+++ 02-conditional-branching/Program.fs
@@ -0,0 +1,9 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System
+
+[<EntryPoint>]
+let main argv =
+    let person = argv.[0]
+    printfn "Hello from %s" person
+    0
